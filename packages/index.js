8d5a5a6bec96fd2b3c060f3263961ed1f9c7ff86
jiangfei03 <jiangfei03@daojia-inc.com>
2023-03-30 16:19:46 +0800

    feat: compatible with umd

--- packages/index.js
+++ packages/index.js
@@ -5,6 +5,11 @@ const install = function (Vue) {
   Vue.component(ElColGroup.name, ElColGroup);
 };
 
+// 判断是否是直接引入文件
+if (typeof window !== "undefined" && window.Vue) {
+  install(window.Vue);
+}
+
 export default {
   install,
   ElColGroup,
